3ab58f12c7495c90dfb66baf5415398e7ec15402
amber-h <amber.houle3@gmail.com>
2013-09-05 14:54:20 -0400

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PhotogurFromScratch::Application.config.secret_token = 'a39160fd130b7ca80833eab234398853bab28d28b8c20f7e04a4a18193c9ee6cb8a23891f364bbaefaa4cd6b825f7835a4e19c22bc9107a5b2dee3e8251aa8cb'
